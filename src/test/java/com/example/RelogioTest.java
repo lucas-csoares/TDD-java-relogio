7db8921817330c3586b57e50489d7564eec11559
Lucas Soares <polonio230@hotmail.com>
2023-08-29 01:18:26 -0300

    Make folders and define the Relogio and RelogioTeste classes

---
+++ src/test/java/com/example/RelogioTest.java
@@ -0,0 +1,13 @@
+package com.example;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.example.Relogio;
+import org.junit.jupiter.api.Test;
+public class RelogioTest {
+
+    @Test
+    public void testHora() {
+
+
+    }
+}
