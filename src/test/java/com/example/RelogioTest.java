96f32aac2ca15070f1f7f21bc5eebcc42df0db12
Lucas Soares <polonio230@hotmail.com>
2023-08-29 15:31:53 -0300

    Create test scenario for difTime method

--- src/test/java/com/example/RelogioTest.java
+++ src/test/java/com/example/RelogioTest.java
@@ -1,13 +1,17 @@
 package com.example;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.example.Relogio;
 import org.junit.jupiter.api.Test;
+
+
 public class RelogioTest {
 
     @Test
     public void testHora() {
-
-
+        Relogio relogio = new Relogio();
+        String difTime = relogio.difTime("10:30","15:45");
+        assertEquals("5:15", difTime);
     }
 }
+
+
