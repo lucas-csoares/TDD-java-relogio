c4f2a7db6cb4ed34aa4299cd571525613ecfcf3b
Lucas Soares <polonio230@hotmail.com>
2023-08-30 14:54:34 -0300

    Create test scenario for atualizar  method

--- src/test/java/com/example/RelogioTest.java
+++ src/test/java/com/example/RelogioTest.java
@@ -3,25 +3,42 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.example.Relogio;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalTime;
 
 
 public class RelogioTest {
 
     @Test
-    public void testHora() {
+    @DisplayName("Validar método função cronômetro")
+    public void testDifTime() {
         Relogio relogio = new Relogio();
         String difTime = relogio.difTime("10:30:10","15:45:15");
         assertEquals("05:15:05", difTime);
     }
 
     @Test
+    @DisplayName("Validar método de ajuste do relógio")
     public void testAjustar() {
         Relogio relogio = new Relogio();
         assertThrows(IllegalArgumentException.class, () -> {
             relogio.ajustar(-1, -30, -45);
         });
     }
+
+    @Test
+    @DisplayName("Validar método atualizar relógio para hora atual")
+    public void testAtualizarHora() {
+        Relogio relogio = new Relogio();
+        relogio.atualizar();
+        assertEquals(Integer.valueOf(LocalTime.now().getHour()), relogio.getHora());
+        assertEquals(Integer.valueOf(LocalTime.now().getMinute()), relogio.getMinuto());
+        assertEquals(Integer.valueOf(LocalTime.now().getSecond()), relogio.getSegundo());
+    }
+
+
 
 }
 
